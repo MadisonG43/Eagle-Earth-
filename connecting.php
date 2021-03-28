3c5c19d6ca2eaaeaa1dacb4704549ee2b16080e4
Madison Granger <46722075+MadisonG43@users.noreply.github.com>
2021-03-28 13:46:35 -0500

    Create connecting.php

---
+++ connecting.php
@@ -0,0 +1,12 @@
+<?php
+$servername= "localhost:3306";
+$db_username="cchs_mgranger";
+$db_password="eagles";
+$db_database="cchs_mgranger";
+
+$mySQLI = new mysqli($servername, $db_username, $db_password, $db_database);
+if($mySQLI->connect_error){
+    echo("Failed to connect: ".$mySQLI->connect_error);
+    exit();
+}
+?>
